68012d12f10d4f290f154c03e32cabc2d565e786
ABHIRAM <abhirampalika@gmail.com>
2020-11-04 22:37:01 +0530

    hello

--- lib/screens/homedesign.dart
+++ lib/screens/homedesign.dart
@@ -5,7 +5,7 @@ import 'homepage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'signup.dart';
-import 'file:///C:/Users/ABHIRAM/AndroidStudioProjects/pharmatiq/lib/widgets/notification_button.dart';
+import 'package:pharmatiq/widgets/notification_button.dart';
 
 class DashboardPage extends StatefulWidget {
   final String uid;
--- lib/widgets/NavDrawer.dart
+++ lib/widgets/NavDrawer.dart
@@ -1,4 +1,4 @@
-import 'file:///C:/Users/ABHIRAM/AndroidStudioProjects/pharmatiq/lib/screens/profile.dart';
+import 'package:pharmatiq/screens/profile.dart';
 import 'package:pharmatiq/authentication.dart';
 import 'package:pharmatiq/screens/login.dart';
 import '../screens/homedesign.dart';
