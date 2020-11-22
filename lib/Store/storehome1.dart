6456aad049f0cbcb6148611d96e39b9c1a358da3
abhiram <abhirampalika@gmail.com>
2020-11-22 21:21:47 +0530

    hello

---
+++ lib/Store/storehome1.dart
@@ -0,0 +1,295 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:pharmatiq/Store/cart.dart';
+import 'package:pharmatiq/Store/product_page.dart';
+import 'package:pharmatiq/Store/searchBox.dart';
+import 'package:pharmatiq/config.dart';
+import 'package:pharmatiq/item.dart';
+import 'package:pharmatiq/screens/homedesign.dart';
+import 'package:pharmatiq/widgets/cartitemcounter.dart';
+import 'package:pharmatiq/widgets/loadingwidget.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+double width;
+
+class StoreHome1 extends StatefulWidget
+{
+  @override
+  _StoreHome1State createState() => _StoreHome1State();
+}
+
+class _StoreHome1State extends State<StoreHome1>
+{
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar (
+          automaticallyImplyLeading: false,
+          leading:GestureDetector(
+            onTap: () {Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => DashboardPage()));},
+            child: Icon(
+              Icons.arrow_back,
+            ),
+          ),
+          flexibleSpace: Container(
+            decoration: new BoxDecoration(
+              color: Color.fromRGBO(0, 170, 140, 1),
+            ),
+          ),
+          actions: [
+            Stack(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.shopping_cart,color: Colors.black,),
+                  onPressed: ()
+                  {
+                    Route route = MaterialPageRoute(builder: (c) => CartPage());
+                    Navigator.pushReplacement(context, route);
+                  },
+                ),
+                Positioned(
+                  child: Stack(
+                    children: [
+                      Icon(
+                        Icons.brightness_1,
+                        size: 20.0,
+                        color: Colors.black,
+                      ),
+                      Positioned(
+                        top: 3.0,
+                        bottom: 4.0,
+                        child: Consumer<CartItemCounter>(
+                          builder: (context, counter, _)
+                          {
+                            return Text(
+                              ((EcommerceApp.cartList).length-1).toString(),
+                              style: TextStyle(color: Colors.white,fontSize: 12.0,fontWeight: FontWeight.w500),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        body: CustomScrollView(
+          slivers: [
+            SliverPersistentHeader(pinned: true , delegate: SearchBoxDelegate()),
+            StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection("Items1").limit(15).orderBy("publishedDare", descending: true).snapshots(),
+              builder: (context , dataSnapshot)
+              {
+                return !dataSnapshot.hasData
+                    ? SliverToBoxAdapter(child: Center(child: circularProgress(),),)
+                    : SliverStaggeredGrid.countBuilder(
+                  crossAxisCount: 1,
+                  staggeredTileBuilder: (c) => StaggeredTile.fit(1),
+                  itemBuilder: (context , index)
+                  {
+                    ItemModel model =ItemModel.fromJson(dataSnapshot.data.documents[index].data);
+                    return sourceInfo(model , context);
+                  },
+                  itemCount: dataSnapshot.data.documents.length,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+Widget sourceInfo(ItemModel model, BuildContext context,{Color background , removeCartFunction})
+{
+  return InkWell(
+    onTap: ()
+    {
+      Route route = MaterialPageRoute(builder: (c) => ProductPage(itemModel: model));
+      Navigator.pushReplacement(context, route);
+    },
+    splashColor: Colors.black,
+    child: Padding(
+      padding: EdgeInsets.all(6.0),
+      child: Container(
+        height: 190.0,
+        width: width,
+        child: Row(
+          children: [
+            Image.network(model.thumbnailUrl , width: 140.0, height: 140.0,),
+            SizedBox(width: 4.0,),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 15.0,),
+                  Container(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max ,
+                      children: [
+                        Expanded(
+                          child: Text(model.title , style: TextStyle(color: Colors.white , fontSize: 14.0),),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5.0,),
+                  Container(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max ,
+                      children: [
+                        Expanded(
+                          child: Text(model.shortInfo , style: TextStyle(color: Colors.blueGrey, fontSize: 12.0),),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20.0,),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Colors.green
+                        ),
+                        alignment: Alignment.topLeft,
+                        width: 40.0,
+                        height: 43.0,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "50%",style: TextStyle(fontSize: 15.0, color: Colors.white, fontWeight: FontWeight.normal),
+                              ),
+                              Text(
+                                "OFF" ,style: TextStyle(fontSize: 12.0, color: Colors.white, fontWeight: FontWeight.normal),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10.0,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 0.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  r"Original Price: ₹",
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: Colors.blueGrey,
+                                    decoration: TextDecoration.lineThrough,
+                                  ),
+                                ),
+                                Text(
+                                  (model.price + model.price).toString(),
+                                  style: TextStyle(
+                                    fontSize: 15.0,
+                                    color: Colors.blueGrey,
+                                    decoration: TextDecoration.lineThrough,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 5.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  r"New Price: ₹",
+                                  style: TextStyle(
+                                      fontSize: 14.0,
+                                      color: Colors.blueGrey
+                                  ),
+                                ),
+                                Text(
+                                  (model.price + model.price).toString(),
+                                  style: TextStyle(
+                                      fontSize: 14.0,
+                                      color: Colors.blueGrey
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Flexible(
+                    child: Container(
+
+                    ),
+                  ),
+                  //to implement the cart item remove feature
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: removeCartFunction == null
+                        ? IconButton(
+                      icon: Icon(Icons.add_shopping_cart , color:  Colors.green,),
+                      onPressed: ()
+                      {
+                        checkItemInCart(model.shortInfo , context);
+                      },
+                    )
+                        : IconButton(
+                      icon: Icon(Icons.delete, color: Colors.green,),
+                      onPressed: (){
+                        removeCartFunction();
+                        Route route =MaterialPageRoute(builder: (c) => StoreHome1());
+                        Navigator.pushReplacement(context, route);
+                      },
+                    ),
+                  ),
+                  Divider(
+                    height: 5.0,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget card({Color primaryColor = Colors.redAccent, String imgPath}){
+  return Container();
+}
+
+void checkItemInCart(String shortInfoAsId , BuildContext context)
+{
+  print(EcommerceApp.cartList);
+  EcommerceApp.cartList.contains(shortInfoAsId)
+      ? Fluttertoast.showToast(msg: "Item is already in Cart")
+      : addItemToCart(shortInfoAsId, context);
+}
+
+addItemToCart(String shortInfoId, BuildContext context)
+{
+  List tempCartList = EcommerceApp.cartList;
+  tempCartList.add(shortInfoId);
+
+  Firestore.instance.collection("users")
+      .document(EcommerceApp.userUID)
+      .updateData({
+    EcommerceApp.userCartList : tempCartList,
+  }).then((v){
+    Fluttertoast.showToast(msg: "Item Addd To Cart Successfully.");
+    EcommerceApp.cartList = tempCartList;
+    Provider.of<CartItemCounter>(context , listen: false).displayResult();
+  });
+}
---
+++ lib/screens/uploaditems2.dart
@@ -0,0 +1,290 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:pharmatiq/screens/adminLogin.dart';
+import 'package:pharmatiq/screens/adminshiftorders.dart';
+import 'package:pharmatiq/widgets/loadingwidget.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:pharmatiq/item2.dart';
+
+class UploadPage2 extends StatefulWidget{
+  @override
+  _UploadPage2State createState() => _UploadPage2State();
+}
+
+class _UploadPage2State extends State<UploadPage2> with AutomaticKeepAliveClientMixin<UploadPage2>
+{
+  bool get wantKeepAlive => true;
+  File file;
+  TextEditingController _descripitionTextEditingController = TextEditingController();
+  TextEditingController _priceTextEditingController = TextEditingController();
+  TextEditingController _titleTextEditingController = TextEditingController();
+  TextEditingController _shortInfoTextEditingController = TextEditingController();
+  String productId = DateTime.now().millisecondsSinceEpoch.toString();
+  bool uploading = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return file == null ? displayAdminHomeScreen() : displayAdminUploadFormcreen();
+  }
+  displayAdminHomeScreen()
+  {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: new BoxDecoration(
+            color: Color.fromRGBO(0, 170, 140, 1),
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.border_color , color: Colors.white,),
+          onPressed: ()
+          {
+            Route route = MaterialPageRoute(builder: (c) => AdminShiftOrders());
+            Navigator.pushReplacement(context, route);
+          },
+        ),
+        actions: [
+          FlatButton(
+            child: Text("Logout",style: TextStyle(
+              color: Colors.white,
+              fontSize: 15.0,
+            ),),
+            onPressed: ()
+            {
+              Route route = MaterialPageRoute(builder: (c) => AdminPage());
+              Navigator.pushReplacement(context, route);
+            },
+          ),
+        ],
+      ),
+      body: getAdminHomeScreenBody(),
+    );
+  }
+  getAdminHomeScreenBody()
+  {
+    return Container(
+      decoration: new BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.shop_two, color: Colors.green, size: 200.0,),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9.0)),
+                child: Text("Add New Items",style: TextStyle(fontSize: 20.0 ,color: Colors.black),),
+                color: Colors.green,
+                onPressed: () => takeImage(context),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  takeImage(mContext)
+  {
+    return showDialog(
+        context: mContext,
+        builder: (con)
+        {
+          return SimpleDialog(
+            title: Text("Item Image" , style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
+            children: [
+              SimpleDialogOption(
+                child : Text("Capture With Camera",style: TextStyle(color: Colors.green ,),),
+                onPressed: capturePhotoWithCamera,
+              ),
+              SimpleDialogOption(
+                child : Text("Select From Gallery",style: TextStyle(color: Colors.green ,),),
+                onPressed: pickPhotoFromGallery,
+              ),
+              SimpleDialogOption(
+                child : Text("Cancel",style: TextStyle(color: Colors.green ,),),
+                onPressed: ()
+                {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+  capturePhotoWithCamera() async
+  {
+    Navigator.pop(context);
+    File imageFile = await ImagePicker.pickImage(source: ImageSource.camera, maxHeight: 680.0, maxWidth: 970.0);
+    setState(() {
+      file = imageFile;
+    });
+  }
+
+  pickPhotoFromGallery() async
+  {
+    Navigator.pop(context);
+    File imageFile = await ImagePicker.pickImage(source: ImageSource.gallery,);
+    setState(() {
+      file = imageFile;
+    });
+  }
+
+  displayAdminUploadFormcreen()
+  {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: new BoxDecoration(
+            color: Color.fromRGBO(0, 170, 140, 1),
+          ),
+        ),
+        leading: IconButton(icon: Icon(Icons.arrow_back,color: Colors.white,),onPressed: clearFormInfo),
+        title: Text("New Product", style: TextStyle(color: Colors.white,fontSize: 24.0,fontWeight: FontWeight.bold),),
+        actions: [
+          FlatButton(
+            onPressed: uploading ? null : () =>  uploadImageAndSaveItemInfo(),
+            child: Text("Add" ,style: TextStyle(color: Colors.white,fontSize: 16.0,fontWeight: FontWeight.bold),),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          uploading ? circularProgress() : Text(""),
+          Container(
+            height: 230.0,
+            width: MediaQuery.of(context).size.width*0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16/9,
+                child: Container(
+                  decoration: BoxDecoration(image: DecorationImage(image: FileImage(file), fit: BoxFit.cover)),
+                ),
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 12.0),),
+          ListTile(
+            leading: Icon(Icons.perm_device_information , color: Colors.green,),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                style: TextStyle(color: Colors.black),
+                controller: _shortInfoTextEditingController,
+                decoration: InputDecoration(
+                    hintText: "Short Info",
+                    hintStyle: TextStyle(color: Colors.black),
+                    border: InputBorder.none
+                ),
+              ),
+            ),
+          ),
+          Divider(color: Colors.green,),
+          ListTile(
+            leading: Icon(Icons.perm_device_information , color: Colors.green,),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                style: TextStyle(color: Colors.black),
+                controller: _titleTextEditingController,
+                decoration: InputDecoration(
+                    hintText: "Title",
+                    hintStyle: TextStyle(color: Colors.black),
+                    border: InputBorder.none
+                ),
+              ),
+            ),
+          ),
+          Divider(color: Colors.green,),
+          ListTile(
+            leading: Icon(Icons.perm_device_information , color: Colors.green,),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                style: TextStyle(color: Colors.black),
+                controller: _descripitionTextEditingController,
+                decoration: InputDecoration(
+                    hintText: "Description",
+                    hintStyle: TextStyle(color: Colors.black),
+                    border: InputBorder.none
+                ),
+              ),
+            ),
+          ),
+          Divider(color: Colors.green,),
+          ListTile(
+            leading: Icon(Icons.perm_device_information , color: Colors.green,),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                keyboardType: TextInputType.number,
+                style: TextStyle(color: Colors.black),
+                controller: _priceTextEditingController,
+                decoration: InputDecoration(
+                    hintText: "Price",
+                    hintStyle: TextStyle(color: Colors.black),
+                    border: InputBorder.none
+                ),
+              ),
+            ),
+          ),
+          Divider(color: Colors.green,)
+        ],
+      ),
+    );
+  }
+  clearFormInfo()
+  {
+    setState(() {
+      file = null;
+      _descripitionTextEditingController.clear();
+      _priceTextEditingController.clear();
+      _shortInfoTextEditingController.clear();
+      _titleTextEditingController.clear();
+    });
+  }
+  uploadImageAndSaveItemInfo() async
+  {
+    setState(() {
+      uploading = true;
+    });
+    String imageDownloadUrl = await uploadItemImage(file);
+    saveItemInfo(imageDownloadUrl);
+  }
+  Future<String> uploadItemImage(mFileImage) async
+  {
+    final StorageReference storageReference = FirebaseStorage.instance.ref().child("Items2");
+    StorageUploadTask uploadTask = storageReference.child("product_$productId.jpg").putFile(mFileImage);
+    StorageTaskSnapshot takeSnapshot = await uploadTask.onComplete;
+    String downloadUrl = await takeSnapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+  saveItemInfo(String downloadUrl)
+  {
+    final itemsRef = Firestore.instance.collection("Items2");
+    itemsRef.document(productId).setData({
+      "shortInfo" : _shortInfoTextEditingController.text.trim(),
+      "longDescription" : _descripitionTextEditingController.text.trim(),
+      "price" : int.parse(_priceTextEditingController.text),
+      "publishedDare" : DateTime.now(),
+      "status" : "available",
+      "thumbnailUrl" : downloadUrl,
+      "title" : _titleTextEditingController.text.trim(),
+    });
+    setState(() {
+      file = null;
+      uploading = false;
+      productId = DateTime.now().millisecondsSinceEpoch.toString();
+      _descripitionTextEditingController.clear();
+      _priceTextEditingController.clear();
+      _shortInfoTextEditingController.clear();
+      _titleTextEditingController.clear();
+    });
+  }
+}
